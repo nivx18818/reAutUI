be50fe8e11d4fa21ab37938ff3b55fe6c936341d
NguyenXuanVinh <nongami87@gmail.com>
2025-05-21 22:13:09 +0700

    feat: implement form submission for scenario updates

--- src/pages/Scenario/index.jsx
+++ src/pages/Scenario/index.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router";
-import { useCurrentScenario } from "@/hooks/useScenario";
+import useScenario, { useCurrentScenario } from "@/hooks/useScenario";
+import httpRequest from "@/utils/httpRequest";
+
 import { Database } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -7,10 +9,30 @@ import { Textarea } from "@/components/ui/textarea";
 
 function Scenario() {
   const navigate = useNavigate();
+  const { updateScenarioInContext } = useScenario();
   const currentScenario = useCurrentScenario();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const updatedScenarioData = {
+      ...currentScenario,
+      ...Object.fromEntries(formData),
+    };
+    await httpRequest.put(
+      `/scenarios/${currentScenario.id}`,
+      updatedScenarioData
+    );
+    updateScenarioInContext(updatedScenarioData);
+    navigate("../test-data");
+  };
+
   return (
-    <div key={currentScenario?.id} className="max-w-2xl mx-auto space-y-6">
+    <form
+      key={currentScenario?.id}
+      className="max-w-2xl mx-auto space-y-6"
+      onSubmit={handleSubmit}
+    >
       <label className="block space-y-2">
         <span className="text-sm font-medium">URL</span>
         <Input
@@ -23,18 +45,18 @@ function Scenario() {
       <label className="block space-y-2">
         <span className="text-sm font-medium">Description</span>
         <Textarea
-          name="scenarioDescription"
+          name="description"
           placeholder="Enter detailed description of the scenario"
           defaultValue={currentScenario?.description}
           className="resize-none min-h-40"
         />
       </label>
 
-      <Button className="w-full" onClick={() => navigate("../test-data")}>
+      <Button className="w-full">
         <Database />
         <span>INPUT TEST DATA</span>
       </Button>
-    </div>
+    </form>
   );
 }
 
